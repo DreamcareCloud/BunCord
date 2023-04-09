81e7a92373603d697745ff71878ee3e3fcec431f
LittlestFluffy <363388+LittlestFluffy@users.noreply.github.com>
2023-04-10 00:53:13 +0200

    added some arguments for testing purposes

--- src/main/kotlin/cloud/dreamcare/buncord/commands/TestCommand.kt
+++ src/main/kotlin/cloud/dreamcare/buncord/commands/TestCommand.kt
@@ -4,16 +4,28 @@ import cloud.dreamcare.buncord.dsl.command
 import cloud.dreamcare.buncord.dsl.event
 import dev.kord.core.behavior.interaction.response.respond
 import dev.kord.core.entity.application.GlobalChatInputCommand
+import dev.kord.core.entity.interaction.IntegerOptionValue
+import dev.kord.core.entity.interaction.RoleOptionValue
 import dev.kord.core.event.interaction.ChatInputCommandInteractionCreateEvent
+import dev.kord.rest.builder.interaction.integer
+import dev.kord.rest.builder.interaction.role
 import kotlinx.coroutines.Job
+import mu.KLogger
+import mu.KotlinLogging
 
 public class TestCommand {
-    public suspend fun registerA(): GlobalChatInputCommand = command("one") {}
+    private val logger: KLogger = KotlinLogging.logger { }
 
+    public suspend fun registerA(): GlobalChatInputCommand = command("one") {
+        integer("number", "pwetty number")
+        role("role", "select your role")
+    }
     public suspend fun registerB(): GlobalChatInputCommand = command("two") {}
 
     public suspend fun testA(): Job = event<ChatInputCommandInteractionCreateEvent>("one") {
         val response = interaction.deferPublicResponse()
+        logger.info { (interaction.command.options["number"] as IntegerOptionValue).value }
+        logger.info { (interaction.command.options["role"] as RoleOptionValue).resolvedObject?.name }
 
         response.respond { content = "Hi number one!" }
     }
