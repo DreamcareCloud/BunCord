dce0af9efc06d084b26d3aa5539f1b0d4a5f48df
LittlestFluffy <363388+LittlestFluffy@users.noreply.github.com>
2023-04-18 18:56:38 +0200

    updated gradle to 8.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,11 @@
 plugins {
-    application
-    kotlin("jvm") version "1.8.20"
+    kotlin("multiplatform") version "1.8.20"
     id("com.palantir.git-version") version "3.0.0"
+    application
 }
 
 application {
     mainClass.set("cloud.dreamcare.buncord.BunCordKt")
-}
-
-kotlin {
-    jvmToolchain(17)
-    explicitApi()
 }
 
 val gitVersion: groovy.lang.Closure<String> by extra
@@ -20,32 +15,39 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    // Discord
-    implementation("dev.kord:kord-core:0.8.3")
+kotlin {
+    explicitApi()
+    jvm {
+        jvmToolchain(17)
+        withJava()
+    }
+    sourceSets {
+        all {
+            languageSettings {
+                languageVersion = "2.0"
+            }
+        }
+        named("jvmMain") {
+            dependencies {
+                // DotEnv loader
+                implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
+                implementation("ch.qos.logback:logback-classic:1.4.6")
 
-    // Logging
-    implementation("io.github.oshai", "kotlin-logging-jvm", "4.0.0-beta-28")
-    implementation("ch.qos.logback", "logback-classic", "1.4.6")
-
-    // Tools
-    implementation("io.github.cdimascio", "dotenv-kotlin", "6.4.1")
-    implementation("org.reflections", "reflections", "0.10.2")
+                implementation("dev.kord:kord-core:0.8.3")
+                implementation("io.github.oshai:kotlin-logging-jvm:4.0.0-beta-28")
+                implementation("org.reflections:reflections:0.10.2")
+            }
+        }
+    }
 }
 
-tasks {
-    val fatJar = register<Jar>("fatJar") {
-        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-        dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources"))
-        archiveClassifier.set("standalone")
+tasks.named<Jar>("jvmJar") {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    archiveClassifier.set("standalone")
 
-        manifest { attributes(mapOf("Main-Class" to application.mainClass)) }
-        from(sourceSets.main.get().output)
-        from({
-            configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
-        })
-    }
-    build {
-        dependsOn(fatJar)
-    }
+    manifest { attributes(mapOf("Main-Class" to application.mainClass)) }
+    from(sourceSets.main.get().output)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
 }
